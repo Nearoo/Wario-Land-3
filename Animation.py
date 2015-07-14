57b7f84306ccfb2a1d9cb91f7d9dcbf602f843ee
Silas Gyger <silasgyger@gmail.com>
2015-07-14 21:14:34 +0200

    #1: Improve animation class to support new order control argument format

--- Animation.py
+++ Animation.py
@@ -1,43 +1,51 @@
-import copy
 from globals import pygame
+
 
 class Animation:
 	"""
-	The animation class is very usefull to handle sprite-animations.
+	The animation class is very useful to handle sprite-animations.
 	It is recommended to create a class that inherits from this one and then 
-	change the _costom()-method to add costom behaviour.
+	change the _custom()-method to add custom behaviour.
 
 	Most methods who aren't getters return self in order to perform actions like
 	animation.pause().reset().get_surface()
 
 	"""
-	def __init__(self, sprites, frames_per_image):
+	def __init__(self, sprites, sprite_order):
 		"""
 		sprites: A list of images of the sprite, each image is a frame of the animation.
 			The images all have to have the same size.
 			See utilities.split_tiled_surface() for an easy way to generate such a list.
 
-			Sprites can't have an alpha-channel, transparents parts must be indicated by the
+			Sprites can't have an alpha-channel, transparent parts must be indicated by the
 			color (225, 0, 225)
 
-		frame_per_image: Determines the general speed of the animaion.
-			The number is the frame each image is show on screen, so frame-rate is
-			1/frames_per_image - and int.
+		sprite_order: Can be three of the following things:
+			Amount (int) of frames every sprite is showed. The order of the sprites played will be the order of the
+			sprites in the "sprites"-argument.
 
 			OR
 
-			A list with the speed of each frame. ([frame_for_image_1, frame_for_image_2...])
-			Both work.
+			The amount of frames for every sprites (list). The order of the sprites played will be the order of the
+			sprites in the "sprites"-argument, but this time, the amount of frames each sprite is showed can be changed
+			individually. ([frames_for_image_1, frames_for_image_2...])
+
+			OR
+
+			The order of the sprites and the amount of frames for every sprite (list of tuples). This list can contain
+			more elements than there are sprites. The syntax for this list is [(sprite_nr, frame_length),
+			(sprite_nr, frame_length)...].
+			An animation with three different sprites can be 5 sprites long,
+			playing sprite |: 1, 2, 1, 3, 2 :| for 100 frames per sprite for instance would be expressed as follows:
+			Animation([surface_1, surface_2, surface_3], [(1, 100), (2, 100), (1, 100), (3, 100), (1, 100)])
 		"""
 
 		# Save the sprites-list
 		self.sprites = sprites
 
-		# Save the animation-speed:
-		self.frames_per_image = frames_per_image
-
-		# Make sure self.frames_per_image is a list containg the speed for every frame, (convert int to list)
-		self.convert_frames_per_image_to_list()
+		# Save the speed and order of sprites according to the type of the given list:
+		self.sprite_order = sprite_order
+		self.format_sprite_order()
 
 		# Initialize the frame_counter
 		self.frame_counter = 0
@@ -66,7 +74,7 @@ class Animation:
 
 	def play(self):
 		"""
-		Plays the animation, meaning suface and frame-number get updated over time
+		Plays the animation, meaning surface and frame-number get updated over time
 		"""
 
 		self.playb = True
@@ -76,7 +84,7 @@ class Animation:
 		"""
 		Pauses the animation.
 		"""
-		self.pause
+		self.playb = False
 		return self
 
 	def reset(self):
@@ -99,10 +107,10 @@ class Animation:
 		The speed is frames per image, so the framerate is 1/speed.
 		"""
 		# Update the animation-speed:
-		self.frames_per_image = frames_per_image
+		self.sprite_order = frames_per_image
 
-		# Make sure self.frames_per_image is a list containg the speed for every frame, (convert int to list)
-		self.convert_frames_per_image_to_list()
+		# Make sure self.sprite_order is a list containing the speed for every frame, (convert int to list)
+		self.format_sprite_order()
 
 		return self
 
@@ -114,51 +122,52 @@ class Animation:
 			sprite.set_colorkey(color)
 		return self
 
-	def _costom(self):
+	def _custom(self):
 		"""
 		This function is called at the same time update() is called.
 		It is used by classes who inherit from this one to add custom behaviour.
 		"""
 		pass
 
-	def convert_frames_per_image_to_list(self):
-		"""Converts a single int that represents the frames_per_image for every frame
-		to a list with the same length as self.sprites containing only this value.
-
-		Used to make it easier to create a simple animation. Instead of Animation(sprites, [100, 100, 100, ....])
-		only Animation(sprites, 100) is needed.
-		"""
-
-		if type(self.frames_per_image) == int:
-			self.frames_per_image = [self.frames_per_image for sprite in self.sprites]
+	def format_sprite_order(self):
+		if type(self.sprite_order) is list:
+			if type(self.sprite_order[0]) is tuple:
+				# sprite_order is already formatted:
+				self.sprite_order = self.sprite_order
+			else:
+				# sprite_order consists of amounts of frames for sprites:
+				self.sprite_order = \
+					[(sprite, length) for sprite, length in zip(range(len(self.sprites)), self.sprite_order)]
+		else:
+			# sprite_order consists of one amount of frames for every sprite:
+			self.sprite_order = [(sprite, self.sprite_order) for sprite in range(len(self.sprites))]
 
 	def update(self):
 		"""
 		This method must be called every frame.
-		It updates the entre animation-object.
+		It updates the entire animation-object.
 		"""
-		# Execute the costom update method used by inherited classes:
-		self._costom()
+		# Execute the custom update method used by inherited classes:
+		self._custom()
 		# Make sure frames_per_image is a list:
-		self.convert_frames_per_image_to_list()
+		self.format_sprite_order()
 		# If animation is currently playing, increase frame_counter
 		if self.playb:
 			self.frame_counter += 1
 
 		# If the current frame-number exceeds the speed frames_per_image...
-		if self.frame_counter >= self.frames_per_image[self.current_sprite]:
+		if self.frame_counter >= self.sprite_order[self.current_sprite][1]:
+			# Increase the index of the current sprite
+			self.current_sprite += 1
+			# If the index of the current frame is higher than the length of sprites available, reset it to 0
+			if self.current_sprite >= len(self.sprite_order):
+				self.current_sprite = 0
 			# Clean up the surface
 			self.surface.blit(self.empty_surface, (0, 0))
 			# Blit the image of the new frame onto the surface
-			self.surface.blit(self.sprites[self.current_sprite], (0, 0))
+			self.surface.blit(self.sprites[self.sprite_order[self.current_sprite][0]], (0, 0))
 			# Reset the frame_counter
 			self.frame_counter = 0
-
-			# Increase the index of the current frame for the next update
-			self.current_sprite += 1
-			# If the index of the current frame is higher than the lenght of sprites aviable, reset it to 0
-			if self.current_sprite >= len(self.sprites):
-				self.current_sprite = 0
 		return self
 
 	def get_spritenr(self):
@@ -176,23 +185,24 @@ class Animation:
 	def make_x_mirror(self):
 		"""
 		Mirrors every image of the animation on the x axis.
-		Usefull for e.g. walk- or jump-animations.
+		Useful for e.g. walk- or jump-animations.
 		"""
 		new_sprites = [pygame.transform.flip(surf, 1, 0) for surf in self.sprites]
-		return self.copy(new_sprites, self.frames_per_image)
+		return self.copy(new_sprites, self.sprite_order)
 
 	def make_y_mirror(self):
 		"""
 		Mirrors every image of the animation on the y axis.
-		Usefull for e.g. walk- or jump-animations.
+		Useful for e.g. walk- or jump-animations.
 		"""
 		new_sprites = [pygame.transform.flip(surf, 0, 1) for surf in self.sprites]
-		return self.copy(new_sprites, self.frames_per_image)
+		return self.copy(new_sprites, self.sprite_order)
+
 	def make_xy_mirror(self):
 		"""
 		Mirrors every image of the animation on the x- and the y-axis.
-		Usefull for e.g. walk- or jump-animations.
+		Useful for e.g. walk- or jump-animations.
 		"""
 
 		new_sprites = [pygame.transform.flip(surf, 1, 1) for surf in self.sprites]
-		return self.copy(new_sprites, self.frames_per_image)
+		return self.copy(new_sprites, self.sprite_order)
