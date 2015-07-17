91fa36785e0e09ce002d39262661d295f49a508e
Silas Gyger <silasgyger@gmail.com>
2015-07-17 16:58:33 +0200

    Issue #23: Just added documentation to base-tile

--- Tiles.py
+++ Tiles.py
@@ -3,6 +3,31 @@ from Animation import *
 
 
 class BaseTile(GameActor):
+	""" Basically just a surface and the property material_group, which defines the
+	behaviour of the Tile. There are following types:
+
+	-solid: Just a normal, solid, unbreakable block.
+	-deco: No physics, only decoration (background mostly)
+
+	-soft_break Breakable with the soft punch, jump and fall-attack
+	-hard_break: Breakable with the hard punch, jump and fall-attack
+	-shot_break: Breakable throwing an enemy (after charging up)
+	-fire_break: Breakable when Wario is on fire (literally)
+
+	-ladder: A material on which Wario can climb in all directions
+
+	-water_still: Still water
+	-water_left: Water that flows to the left
+	-water_right: Water that flows to the right
+	-water_up: Water that flows up
+	-water_down: Water that flows down
+	-water_impervious: Water that is impervious (Wario can't dive in it, only swim on top of it)
+
+	-platform_fallthrough: A platform through which can be jumped if coming from the bottom up
+		Is also pervious in certain states of Wario, like the zombie-wario.
+
+	Note: The transparency color is always (225, 0, 255), also called "magic-pink"
+	"""
 	def __init__(self, position, engine, material_group, tiles_list):
 		super(BaseTile, self).__init__(position, engine)
 		# Set material_group (only for physics, see class description)
