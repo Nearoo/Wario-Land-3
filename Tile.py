f53ff067ebf94726fdc7a71bfdbe7d3c107fa530
Silas Gyger <silasgyger@gmail.com>
2015-07-17 02:16:21 +0200

    Issue #21: Cleaned up Tile a bit

--- Tile.py
+++ Tile.py
@@ -2,12 +2,13 @@ import copy
 from globals import pygame
 from Animation import *
 
+
 class Tile(object):
 	""" Basically just a surface and the property material_group, which defines the
 	behaviour of the Tile. There are following types:
 
 	-solid: Just a normal, solid, unbreakable block.
-	-none: No physics, only decoration (background mostly)
+	-deco: No physics, only decoration (background mostly)
 
 	-soft_break Breakable with the soft punch, jump and fall-attack
 	-hard_break: Breakable with the hard punch, jump and fall-attack
@@ -23,17 +24,18 @@ class Tile(object):
 	-water_down: Water that flows down
 	-water_impervious: Water that is impervious (Wario can't dive in it, only swim on top of it)
 
-	-platform_fallthrough: A platform through which can be jumpt if coming from the bottom up
-		Is also pervious in certain states of Wario.
+	-platform_fallthrough: A platform through which can be jumped if coming from the bottom up
+		Is also pervious in certain states of Wario, like the zombie-wario.
 
-	Note: The transparancy color is always (225, 0, 255), also called "magic-pink"
+	Note: The transparency color is always (225, 0, 255), also called "magic-pink"
 	 """
 	def __init__(self, material_group, tiles_list, colorkey=(225, 0, 225), fpi = 10):
-		# Set material_group (only for phyics, see claa description)
+		# Set material_group (only for physics, see class description)
 		self.material_group = material_group
 		# Create the animation-instance containing all surfaces
 		self.animation = Animation(tiles_list, fpi)
 		self.animation.update()
+		# Create owm rect
 		self.rect = pygame.Rect((0, 0), tiles_list[0].get_size())
 
 	def update(self):
@@ -41,9 +43,6 @@ class Tile(object):
 
 	def get_surface(self):
 		return self.animation.get_surface()
-
-	def get_solid(self):
-		return solid
 
 	def get_rect(self):
 		return self.rect
